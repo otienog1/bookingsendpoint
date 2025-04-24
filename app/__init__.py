1f241f4934b851011aa2e39b1b065ac04e1cf7a7
George Oluoko <otienog1@gmail.com>
2025-04-24 17:16:15 +0300

    authentication

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,15 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_migrate import Migrate
 from dotenv import load_dotenv, find_dotenv
+import secrets
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    migrate = Migrate(app, db)
 
     CORS(app)
 
@@ -18,32 +21,24 @@ def create_app():
         "SQLALCHEMY_DATABASE_URI", "sqlite:///dbname.db"
     )
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    
+    # Configure secret key for JWT
+    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", secrets.token_hex(16))
 
     db.init_app(app)
 
     from .bookingsbp import bookingsbp as bookings_blueprint
-
     app.register_blueprint(bookings_blueprint)
-
-    # from .visabp import visabp as visa_blueprint
-    # app.register_blueprint(visa_blueprint)
-
-    # from .forex import forex as forex_blueprint
-    # app.register_blueprint(forex_blueprint)
+    
+    from .authbp import authbp as auth_blueprint
+    app.register_blueprint(auth_blueprint)
 
     return app
 
-
 app = create_app()
 
-from app import db, create_app
-
 with app.app_context():
-    db.create_all()
+
     # db.drop_all()
 
-# ssh -i "C:\Users\7plus8\.ssh\3cxkepair-VA.pem" ubuntu@ec2-3-85-72-145.compute-1.amazonaws.com
-# ssh -i "C:\Users\7plus8\.ssh\3cxkepair-VA.pem" ubuntu@3.85.72.145
-# mysql
-# username: mpesa_root
-# pass: fWB9TgCoZ49htwHY1uphzNjxgTB3LMb8tNoxI
+    db.create_all()
